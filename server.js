cc1d7f6f396f462e845c3cbee6f4563b51ef6509
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-07-31 18:36:14 -0500

    include 404

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const { animals } = require('./data/animals');
+const e = require('express');
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -36,12 +37,27 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+    
 });
 
 app.listen(PORT, () => {
