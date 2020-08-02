6927064f682c692b5b283e7255e58aefcc909438
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-08-01 21:07:20 -0500

    link form and finish html connections

--- server.js
+++ server.js
@@ -103,6 +103,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 
 app.post('/api/animals', (req, res) => {
     // id based on next index of array
